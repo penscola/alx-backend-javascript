aa6edbb8766dd66088bd46f821bc86e6b92640fe
penscola <penscolashackletonfelix@gmail.com>
2024-01-10 18:34:40 +0300

    Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: employeesList,
+      // eslint-disable-next-line no-shadow
+      getNumberOfDepartments(employeesList) {
+        return Object.keys(employeesList).length;
+      },
+    };
+}
+  
