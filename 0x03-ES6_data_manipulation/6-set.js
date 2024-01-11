c80585565d26a8aa578e166965d8d4623dbc1369
penscola <penscolashackletonfelix@gmail.com>
2024-01-11 17:43:01 +0300

    Weak link data structure

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Felix Kiprotich <https://github.com/penscola>
  * @returns {Set}
  */
 export default function setFromArray(array) {
--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a map of grocery names and their quantity.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Felix Kiprotich <https://github.com/penscola>
  * @returns {Map<string, Number>}
  */
 export default function groceriesList() {
